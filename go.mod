6e210bf4330af5e19f21309c487feb319209ea48
misterpuffin <snghaoren@gmail.com>
2024-07-13 10:16:19 +0800

    Use testify mock

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 )
 
 require (
--- internal/users/service_test.go
+++ internal/users/service_test.go
@@ -6,36 +6,50 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+
 	"github.com/misterpuffin/go-rest-api-boilerplate/internal/config"
 	db "github.com/misterpuffin/go-rest-api-boilerplate/internal/db/sqlc"
-
-	"github.com/stretchr/testify/suite"
 )
 
 type ServiceTestSuite struct {
 	suite.Suite
-	service Service
+	service          Service
+	mockedRepository *MockedRepository
 }
 
-type MockRepository struct{}
-
-func (r MockRepository) GetUserByEmail(_ context.Context, _ string) (db.User, error) {
-	return db.User{}, pgx.ErrNoRows
+type MockedRepository struct {
+	mock.Mock
 }
-func (r MockRepository) CreateUser(_ context.Context, params db.CreateUserParams) (db.User, error) {
-	return db.User{ID: uuid.New(), Username: params.Username, Email: params.Email, HashedPassword: params.HashedPassword, Salt: params.Salt}, nil
+
+func (m *MockedRepository) GetUserByEmail(ctx context.Context, email string) (db.User, error) {
+	args := m.Called(ctx, email)
+	return args.Get(0).(db.User), args.Error(1)
+}
+
+func (m *MockedRepository) CreateUser(ctx context.Context, params db.CreateUserParams) (db.User, error) {
+	args := m.Called(ctx, params)
+	user := db.User{ID: uuid.New(), Username: params.Username, Email: params.Email, HashedPassword: params.HashedPassword, Salt: params.Salt}
+	return user, args.Error(1)
 }
 
 func (s *ServiceTestSuite) SetupSuite() {
-	mockRepository := MockRepository{}
-	s.service = *NewService(*config.LoadTestConfig(), mockRepository)
+	mockedRepository := MockedRepository{}
+	s.service = *NewService(*config.LoadTestConfig(), &mockedRepository)
+	s.mockedRepository = &mockedRepository
 }
 
 func (suite *ServiceTestSuite) TestRegisterUser() {
 	username := "mistermuffin"
 	email := "snghaoren@gmail.com"
 	password := "password"
+
+	suite.mockedRepository.On("GetUserByEmail", mock.Anything, mock.Anything).Return(db.User{}, pgx.ErrNoRows)
+	suite.mockedRepository.On("CreateUser", mock.Anything, mock.Anything).Return(nil, nil)
+
 	user, err := suite.service.RegisterUser(username, email, password)
+
 	suite.Assert().NoError(err)
 	suite.Assert().NotEmpty(user.ID)
 	suite.Assert().Contains(user.Username, username)
